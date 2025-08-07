2391f9b5562c7a0e7c68d3047f894183061966be
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-08-07 20:04:51 +0600

    add create_product.go

---
+++ ecommerce/handlers/create_product.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"ecommerce/database"
+	"ecommerce/utils"
+	"encoding/json"
+	"net/http"
+)
+
+func CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var newProduct database.Product
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&newProduct)
+	if err != nil {
+		http.Error(w, "Please give me valid json", 400)
+		return
+	}
+
+	newProduct.ID = len(database.ProductList) + 1
+	database.ProductList = append(database.ProductList, newProduct)
+
+	utils.SendData(w, newProduct, 201)
+}
