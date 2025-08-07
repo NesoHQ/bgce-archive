1c65d647d0ff95860954b46bdbc36c48d06fdf2b
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-08-07 20:06:36 +0600

    add get_products.go

---
+++ ecommerce/handlers/get_products.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"ecommerce/utils"
+	"ecommerce/database"
+	"net/http"
+)
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(200)
+		return
+	}
+
+	utils.SendData(w, database.ProductList, 200)
+}
