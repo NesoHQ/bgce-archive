974808024ef0201b0d41350bd05c1920163eabea
undefined <105885805+ismymiddlename@users.noreply.github.com>
2025-09-06 11:16:53 +0200

    [cortex] fix(jwt): use standard jwt field to store user id.

--- cortex/cmd/jwt.go
+++ cortex/cmd/jwt.go
@@ -5,7 +5,7 @@ import (
 
 	"cortex/config"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/spf13/cobra"
 )
 
@@ -15,8 +15,8 @@ func GenerateJWTCommand() *cobra.Command {
 		Short: "generate a jwt token",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			conf := config.GetConfig()
-			token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-				Id: "1",
+			token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+				Subject: "1",
 			}).SignedString([]byte(conf.JwtSecret))
 			if err != nil {
 				return err
