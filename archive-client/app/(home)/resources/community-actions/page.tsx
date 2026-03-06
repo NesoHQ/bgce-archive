ffb0def619d0cc5d547bb88f3a51d6a4d124651d
TOMJID <tomjidhuda04@gmail.com>
2026-03-06 16:02:35 +0600

    feat: Define `IContributor` interface for improved type safety, enhance accessibility with image alt attributes, add security to external links, and refine client component directives.

--- archive-client/app/(home)/resources/community-actions/page.tsx
+++ archive-client/app/(home)/resources/community-actions/page.tsx
@@ -1,10 +1,13 @@
-"use client";
-
 import { HeroSection } from "@/components/community/Contributors/HeroSection";
 import { GridWall } from "@/components/community/Contributors/GridWall";
 import { JoinCTA } from "@/components/community/Contributors/JoinCTA";
 
-const contributors = [
+export interface IContributor {
+  name: string;
+  github: string;
+}
+
+const contributors: IContributor[] = [
   { name: "Iqbal Hossain", github: "https://github.com/geomachine" },
   { name: "Nazma Sarker", github: "https://github.com/nazma98" },
   { name: "Ifruin Ruhin", github: "https://github.com/ifrunruhin12" },
@@ -47,7 +50,7 @@ const contributors = [
 
 export default function CommunityActionsPage() {
   return (
-    <div className='y-16 lg:py-24 space-y-20'>
+    <div className='py-16 lg:py-24 space-y-20'>
       {/* Hero Section */}
       <HeroSection contributors={contributors} />
 
