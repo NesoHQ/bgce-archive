8486239394c5ec89759c46df42fec3831c189c2d
saimonsiddique <saimonsiddiquee@gmail.com>
2026-03-26 00:53:00 +0600

    fix: switch service dependency from ContentChecker to Moderator

--- community/comment/svc.go
+++ community/comment/svc.go
@@ -3,7 +3,7 @@ package comment
 import "community/moderation"
 
 // NewService creates a new comment service with injected dependencies
-func NewService(repo Repository, checker moderation.ContentChecker) Service {
+func NewService(repo Repository, checker moderation.Moderator) Service {
 	return &service{
 		repo:    repo,
 		checker: checker,
